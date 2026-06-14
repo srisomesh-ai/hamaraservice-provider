3ffa8a2e3d4d01d88d85895ea8d094efd5d6aa01
srisomesh-ai <somesh9346220090@gmail.com>
2026-06-14 17:36:46 +0200

    feat: load service prices from Firebase at startup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'services/service_price_service.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'utils/theme.dart';
 import 'screens/splash_screen.dart';
@@ -38,6 +39,8 @@ void main() async {
       sound: true,
     );
 
+    // Load service prices from Firebase (admin-controlled)
+    ServicePriceService().loadPrices();
     // Save FCM token to Firebase so server can send push notifications
     final token = await FirebaseMessaging.instance.getToken();
     if (token != null) {
